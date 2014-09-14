49de88c948490b439e8e86bb55878e53a0f4981a
funnythingz <funnythingz69+github@gmail.com>
2014-09-15 00:26:24 +0900

    can post when press enter key

--- modoki.js
+++ modoki.js
@@ -2,27 +2,37 @@ Tweets = new Mongo.Collection("tweets");
 
 if (Meteor.isClient) {
 
-    Template.hello.helpers({
-        title: function () {
-                   return "title";
-               }
-    });
+  Template.hello.helpers({
+    title: function () {
+             return "title";
+           }
+  });
 
-    Template.timeline.helpers({
-        tweets: function() {
-                    return Tweets.find({});
-                }
-    });
-
-    Template.hello.events({
-        'click #post': function (e) {
-            e.preventDefault();
-            if(_.isEmpty($('#tweet').val())) {
-                alert("いれてね");
-            } else {
-                Tweets.insert({tweet: $('#tweet').val()});
-                $('#tweet').val('')
+  Template.timeline.helpers({
+    tweets: function() {
+              return Tweets.find({});
             }
-        }
-    });
+  });
+
+  var postTweet = function() {
+    if(_.isEmpty($('#tweet').val())) {
+      alert("いれてね");
+    } else {
+      Tweets.insert({tweet: $('#tweet').val()});
+      $('#tweet').val('')
+    }
+  }
+
+  Template.hello.events({
+    'keypress #tweet': function(e) {
+      if(_.isEqual(e.keyCode, 13)) {
+        postTweet();
+      }
+    },
+
+    'click #post': function (e) {
+      e.preventDefault();
+      postTweet();
+    }
+  });
 }
